118e4394cf3a28e8a915639c155d9123fdc635e9
Mark Wu <wuhsinyee@hotmail.com>
2016-04-03 01:38:28 +0800

    Change sha1 to sha256

--- Formula/kindlegen.rb
+++ Formula/kindlegen.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Kindlegen < Formula
   homepage 'http://www.amazon.com/gp/feature.html?docId=1000765211'
   url 'http://kindlegen.s3.amazonaws.com/KindleGen_Mac_i386_v2_9.zip'
-  sha1 'efa5ad0b05650f7f71543535ea2b232bb5fec571'
+  sha256 '7aad3b01c8f0d61096f86d7ba34e4deeef22355b9dbf2555c6dd05a281f5d17e'
 
   def install
     bin.install 'kindlegen'
