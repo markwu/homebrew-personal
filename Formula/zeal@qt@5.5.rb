3c73b94c0d68c6c289c3b41d0aeca8b429a59f09
Mark Wu <wuhsinyee@hotmail.com>
2018-12-03 10:21:45 +0800

    Make zeal build with qt 5.11.1

--- Formula/zeal@qt@5.5.rb
+++ Formula/zeal@qt@5.5.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class Zeal < Formula
+class ZealATQtAT55 < Formula
   desc 'Zeal is a simple offline documentation browser inspired by Dash.'
   homepage "http://zealdocs.org/"
   url 'https://github.com/zealdocs/zeal/archive/v0.6.1.tar.gz'
@@ -9,7 +9,7 @@ class Zeal < Formula
   version '0.6.1'
 
   depends_on "cmake" => :build
-  depends_on "qt"
+  depends_on "qt@5.5"
   depends_on "libarchive"
 
   patch :DATA
@@ -29,33 +29,19 @@ class Zeal < Formula
 end
 __END__
 diff --git a/src/libs/core/CMakeLists.txt b/src/libs/core/CMakeLists.txt
-index cd212bb..1a46756 100644
+index cd212bb..bd9b903 100644
 --- a/src/libs/core/CMakeLists.txt
 +++ b/src/libs/core/CMakeLists.txt
-@@ -9,9 +9,14 @@ add_library(Core
- 
+@@ -9,9 +9,12 @@ add_library(Core
+
  target_link_libraries(Core Registry Ui)
- 
-+list(APPEND CMAKE_PREFIX_PATH /usr/local/opt/qt)
-+set(Qt5WebKit_DIR /usr/local/opt/qt5-webkit/lib/cmake/Qt5WebKit)
-+set(Qt5WebKitWidgets_DIR /usr/local/opt/qt5-webkit/lib/cmake/Qt5WebKitWidgets)
+
++list(APPEND CMAKE_PREFIX_PATH /usr/local/opt/qt@5.5)
  find_package(Qt5 COMPONENTS Network WebKit Widgets REQUIRED)
  target_link_libraries(Core Qt5::Network Qt5::WebKit Qt5::Widgets)
- 
+
 +list(APPEND CMAKE_PREFIX_PATH /usr/local/opt/libarchive)
 +list(APPEND CMAKE_LIBRARY_PATH /usr/local/opt/libarchive)
  find_package(LibArchive REQUIRED)
  include_directories(${LibArchive_INCLUDE_DIRS})
  target_link_libraries(Core ${LibArchive_LIBRARIES})
-diff --git a/src/libs/ui/CMakeLists.txt b/src/libs/ui/CMakeLists.txt
-index 1fe12ef..ab47341 100644
---- a/src/libs/ui/CMakeLists.txt
-+++ b/src/libs/ui/CMakeLists.txt
-@@ -31,5 +31,7 @@ add_library(Ui
- 
- target_link_libraries(Ui QxtGlobalShortcut Registry)
- 
-+set(Qt5WebKit_DIR /usr/local/opt/qt5-webkit/lib/cmake/Qt5WebKit)
-+set(Qt5WebKitWidgets_DIR /usr/local/opt/qt5-webkit/lib/cmake/Qt5WebKitWidgets)
- find_package(Qt5 COMPONENTS WebKitWidgets REQUIRED)
- target_link_libraries(Ui Qt5::WebKitWidgets)
