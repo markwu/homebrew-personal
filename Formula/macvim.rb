550c26787c4506eb23a6cd81d790f0f4848eec5d
Mark Wu <wuhsinyee@hotmail.com>
2019-12-16 15:31:46 +0800

    No more --with-custom-ruby, just set the HOMEBREW_CUSTOM_RUBY_HOME. Remove python2 support.

--- Formula/macvim.rb
+++ Formula/macvim.rb
@@ -1,106 +1,71 @@
-# Reference: https://github.com/macvim-dev/macvim/wiki/building
 class Macvim < Formula
   desc "GUI for vim, made for macOS"
   homepage "https://github.com/macvim-dev/macvim"
   head "https://github.com/macvim-dev/macvim.git"
 
-  option 'with-custom-ruby', 'Compiled against with custom ruby.'
-
   depends_on :xcode => :build
+  depends_on "pkg-config" => :build
   depends_on "cscope"
   depends_on "lua"
   depends_on "python"
 
-  conflicts_with "vim",
-    :because => "vim and macvim both install vi* binaries"
-
   def install
-    # Avoid issues finding Ruby headers
-    if MacOS.version == :sierra || MacOS.version == :yosemite
-      ENV.delete("SDKROOT")
-    end
+    # Avoid issues finding SDK Ruby headers
+    ENV.delete("SDKROOT")
 
-    # MacVim doesn't have or require any Python package, so unset PYTHONPATH
+    # Use RVM Ruby
+    ENV["PKG_CONFIG_PATH"] = ENV["HOMEBREW_CUSTOM_RUBY_HOME"] + "/lib/pkgconfig"
+    ENV.prepend_path "PATH" , `pkg-config --variable=bindir ruby-2.6`.chomp
+    ENV.append "LDFLAGS", '-L'+`pkg-config --variable=libdir ruby-2.6`.chomp
+    ENV.append "CFLAGS", '-I'+`pkg-config --variable=includedir ruby-2.6`.chomp
+
+    # Use Homebrew Python
+    ENV.prepend_path "PATH", Formula["python"].opt_libexec/"bin"
+
+    # Vim doesn't require any Python package, unset PYTHONPATH.
     ENV.delete("PYTHONPATH")
 
     # make sure that CC is set to "clang"
     ENV.clang
 
-    opts = []
-    if build.with? 'custom-ruby'
-      custom_ruby_cmd = ENV['HOMEBREW_CUSTOM_RUBY_CMD']
-      opts << "--with-ruby-command=#{custom_ruby_cmd}"
-    end
-
-    system "./configure", "--prefix=#{prefix}",
-                          "--with-features=huge",
+    system "./configure", "--prefix=#{HOMEBREW_PREFIX}",
                           "--with-compiledby=Homebrew",
-                          "--with-macarchs=#{MacOS.preferred_arch}",
-                          "--with-local-dir=#{HOMEBREW_PREFIX}",
-                          "--with-tlib=ncurses",
+                          "--with-features=huge",
                           "--enable-multibyte",
-                          "--enable-terminal",
-                          "--enable-cscope",
+                          "--with-macarchs=#{MacOS.preferred_arch}",
                           "--enable-perlinterp",
-                          "--enable-rubyinterp=dynamic",
-                          "--enable-pythoninterp=dynamic",
-                          "--enable-python3interp=dynamic",
-                          "--enable-luainterp=dynamic",
+                          "--enable-rubyinterp",
+                          "--enable-tclinterp",
+                          "--enable-terminal",
+                          "--with-tlib=ncurses",
+                          "--with-compiledby=Homebrew",
+                          "--with-local-dir=#{HOMEBREW_PREFIX}",
+                          "--enable-cscope",
+                          "--enable-luainterp",
                           "--with-lua-prefix=#{Formula["lua"].opt_prefix}",
-                          *opts
-
+                          "--enable-luainterp",
+                          "--enable-python3interp"
     system "make"
-
-    # Patch MacVim buildin vimrc to fix homebrew python3 dynamic link problem
-    app_path = 'src/MacVim/build/Release/MacVim.app'
-    vimrc = "#{buildpath}/#{app_path}/Contents/Resources/vim/vimrc"
-    inreplace vimrc, /^if exists\("&pythonthreedll"\) && exists\("&pythonthreehome"\).*$/m, <<~EOS
-if exists("&pythonthreedll") && exists("&pythonthreehome")
-  if filereadable("/usr/local/Frameworks/Python.framework/Versions/3.7/Python")
-    " Homebrew python 3.7
-    set pythonthreedll=/usr/local/Frameworks/Python.framework/Versions/3.7/Python
-    set pythonthreehome=/usr/local/Frameworks/Python.framework/Versions/3.7
-  elseif filereadable("/opt/local/Library/Frameworks/Python.framework/Versions/3.7/Python")
-    " MacPorts python 3.7
-    set pythonthreedll=/opt/local/Library/Frameworks/Python.framework/Versions/3.7/Python
-    set pythonthreehome=/opt/local/Library/Frameworks/Python.framework/Versions/3.7
-  elseif filereadable("/Library/Frameworks/Python.framework/Versions/3.7/Python")
-    " https://www.python.org/downloads/mac-osx/
-    set pythonthreedll=/Library/Frameworks/Python.framework/Versions/3.7/Python
-    set pythonthreehome=/Library/Frameworks/Python.framework/Versions/3.7
-  endif
-endif
-
-    EOS
-
-    # Add custom ruby dll to MacVim buildin vimrc
-    if build.with? 'custom-ruby'
-      custom_ruby_lib = ENV['HOMEBREW_CUSTOM_RUBY_LIB']
-      Pathname(vimrc).append_lines <<~EOS
-      " Ruby
-      " MacVim is configured by --with-custom-ruby option to use custom ruby version
-      set rubydll=#{custom_ruby_lib}
-      EOS
-    end
 
     prefix.install "src/MacVim/build/Release/MacVim.app"
     bin.install_symlink prefix/"MacVim.app/Contents/bin/mvim"
 
     # Create MacVim vimdiff, view, ex equivalents
     executables = %w[mvimdiff mview mvimex gvim gvimdiff gview gvimex]
-    executables += %w[vi vim vimdiff view vimex]
     executables.each { |e| bin.install_symlink "mvim" => e }
   end
 
   def caveats; <<~EOS
-    To compile MacVim with custom ruby, you have to use '--with-custom-ruby'
-    option, and also specify the following two enironment variables in you shell
-    environment or .bashrc:
+    To compile Vim with custom Ruby, you have to specify the following 
+    HOMEBREW_CUSTOM_RUBY_HOME environment variable in you shell environment:
 
-    $ export HOMEBREW_CUSTOM_RUBY_CMD=$(ruby -r rbconfig -e "print File.join(RbConfig::CONFIG['bindir'], RbConfig::CONFIG['ruby_install_name'])")
-    $ export HOMEBREW_CUSTOM_RUBY_LIB=$(ruby -r rbconfig -e "print File.join(RbConfig::CONFIG['libdir'], RbConfig::CONFIG['LIBRUBY'])")
+    # example: get the prefix from RbConfig
+    $ export HOMEBREW_CUSTOM_RUBY_HOME=$(ruby -r rbconfig -e "print RbConfig::CONFIG['prefix']")
 
-    Without these two variables, custom ruby can not link correctly.
+    # example: get the prefix from RVM environment variable
+    $ export HOMEBREW_CUSTOM_RUBY_HOME=$MY_RUBY_HOME
+
+    Without  HOMEBREW_CUSTOM_RUBY_HOME, custom Ruby can not link correctly.
     EOS
   end
 
