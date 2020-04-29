321a8e990629930ba45177fcf189973eea72c0c2
Mark Wu <wuhsinyee@hotmail.com>
2020-04-29 14:43:23 +0800

    Remove support for zeal 0.61 and qt5-webkit, I only support head version of zealdoc

--- Formula/zeal.rb
+++ Formula/zeal.rb
@@ -3,22 +3,11 @@ require 'formula'
 class Zeal < Formula
   desc 'Zeal is a simple offline documentation browser inspired by Dash.'
   homepage "http://zealdocs.org/"
-  url 'https://github.com/zealdocs/zeal/archive/v0.6.1.tar.gz'
-  sha256 '095c08f9903071849d5c79878abd48237ce1615f16d324afff1873ab6b5f0026'
   head "https://github.com/zealdocs/zeal.git"
-  version '0.6.1'
 
   depends_on "cmake" => :build
   depends_on "qt"
-  depends_on "markwu/personal/qt5-webkit"
   depends_on "libarchive"
-
-  stable do
-    patch do
-      url "https://raw.githubusercontent.com/markwu/homebrew-personal/master/Formula/zeal@0.6.1.diff"
-      sha256 "55bda868aaa3092b84575db5151f910045b650bdf7b6dfe1058cdea91829a810"
-    end
-  end
 
   def install
     mkdir "build" do
